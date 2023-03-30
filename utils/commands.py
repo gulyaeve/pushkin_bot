9bf0c2f344e44dbca02c19091b381bf7d05e4df5
gulyaeve <gulyaev.e@icloud.com>
2023-03-30 21:34:48 +0300

    Added driver commands

--- utils/commands.py
+++ utils/commands.py
@@ -11,6 +11,7 @@ async def set_default_commands():
         # types.BotCommand(command="/start", description="Начать работу с чат-ботом"),
         # types.BotCommand(command="/auth", description="Авторизация"),
         types.BotCommand(command="/taxi", description="Вызвать такси"),
+        types.BotCommand(command="/driver", description="Для водителей"),
         types.BotCommand(command="/promo", description="Узнать акции в Пушкино"),
         types.BotCommand(command="/events", description="Узнать что и где происходит в Пушкино"),
         types.BotCommand(command="/help", description="Помощь по командам чат-бота"),
@@ -21,6 +22,8 @@ async def set_default_commands():
 help_message = """<b>Команды чат-бота:</b>
 
     <b>/taxi</b> - Вызвать такси
+    
+    <b>/driver</b> - Для водителей
     
     <b>/promo</b> - Узнать акции в Пушкино
     
